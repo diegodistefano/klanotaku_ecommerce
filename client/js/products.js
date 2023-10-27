dfc27967ea33413a5e38282ff9bf05990b75b075
diegodistefano <diego.distefano.1989@gmail.com>
2023-10-27 00:30:35 -0300

    Unificando Maquetado con Carrito

---
+++ client/js/products.js
@@ -0,0 +1,44 @@
+const productos = [
+    {
+        id:1,
+        productName: "Remera de One Piece",
+        price: 6000,
+        quanty: 1,
+        img: "/client/media/remera.jpeg",
+    },
+    {
+        id:2,
+        productName: "Figura de Kakashi",
+        price: 15000,
+        quanty: 1,
+        img: "/client/media/figura.webp",
+    },
+    {
+        id:3,
+        productName: "Taza de Star Wars",
+        price: 8000,
+        quanty: 1,
+        img: "/client/media/taza.jpeg",
+    },
+    {
+        id:4,
+        productName: "Gorra de Dragon Ball",
+        price: 5000,
+        quanty: 1,
+        img: "/client/media/gorra.webp",
+    },
+    {
+        id:5,
+        productName: "Buzo de Batman",
+        price: 10000,
+        quanty: 1,
+        img: "/client/media/buzo.png",
+    },
+    {
+        id:6,
+        productName: "Llavero de Capitan America",
+        price: 1000,
+        quanty: 1,
+        img: "/client/media/llavero.webp",
+    },
+];
